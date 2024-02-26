bb5e1ceb84b62c820a667ba735f727e63e8fb36f
alecio70 <alecioccb70@gmail.com>
2024-02-26 16:23:04 -0300

    adding post to the home feed page and adding pagination

--- src/handlers/PostHandler.php
+++ src/handlers/PostHandler.php
@@ -2,6 +2,8 @@
 namespace src\handlers;
 
 use \src\models\Post;
+use \src\models\User;
+use \src\models\UserRelation;
 
 class PostHandler {
 
@@ -15,6 +17,72 @@ class PostHandler {
                 'body' => $body
             ])->execute();
         }
+    }
+
+    public static function getHomeFeed($idUser, $page) {
+        $serPage = 2;
+
+        //1. pegar listar do usuário que EU sigo.
+        $userList = UserRelation::select()->where('user_from', $idUser)->get();
+        $users = [];
+
+        foreach ($userList as $userItem) {
+            $users[] = $userItem['user_to'];
+        }
+
+        $users[] = $idUser;
+
+        //2. pegar os posts dessa galera ordenado pela data.
+        $postList = Post::select()
+            ->where('id_user', 'in', $users)
+            ->orderBy('create_at', 'desc')
+            ->page($page, $serPage)
+            
+        ->get();
+
+        $total = Post::select()
+            ->where('id_user', 'in', $users)
+        ->count();
+
+        $pageCount = ceil($total / $serPage);
+
+        //3. transformar resultado em objetos dos models
+        $posts = [];
+
+        foreach ($postList as $postItem) {
+            $newPost = new Post();
+            $newPost->id = $postItem['id'];
+            $newPost->type = $postItem['type'];
+            $newPost->create_at = $postItem['create_at'];
+            $newPost->body = $postItem['body'];
+            $newPost->mine = false;
+
+            if ($postItem['id_user'] === $idUser) {
+                $newPost->mine = true;
+            }
+
+            //4. preencher as informações adicionais no post
+            $newUser = User::select()->where('id', $postItem['id_user'])->one();
+            $newPost->user = new User();
+            $newPost->user->id = $newUser['id'];
+            $newPost->user->name = $newUser['name'];
+            $newPost->user->avatar = $newUser['avatar'];
+
+            //4.1 preencher as informações de LIKE
+            $newPost->likeCount = 0;
+            $newPost->liked = false;
+            //4.2 preencher as informações de COMMENTS
+            $newPost->comments = [];
+
+            $posts[] = $newPost;
+        }
+
+        //5. retornar o resultado
+        return [
+            'posts' => $posts, 
+            'pageCount' => $pageCount,
+            'currentPage' => $page
+        ];
     }
 
 }
--- src/views/pages/home.php
+++ src/views/pages/home.php
@@ -8,7 +8,18 @@
 
             <?=$render('feed-editor', ['user'=>$loggedUser]);?>
 
-            <?=$render('feed-item');?>
+            <?php foreach ($feed['posts'] as $feedItem): ?>
+                <?=$render('feed-item', [
+                    'data' => $feedItem,
+                    'loggedUser' => $loggedUser
+                ]);?>
+            <?php endforeach ?>
+            
+            <div class="feed-pagination">
+                <?php for ($i=0; $i < $feed['pageCount']; $i++): ?>
+                    <a class="<?=($i==$feed['currentPage'] ? 'active':'');?>" href="<?=$base;?>/?page=<?=$i;?>"><?=$i+1;?></a>
+                <?php endfor; ?>
+            </div>
 
         </div>
         <div class="column side pl-5">
