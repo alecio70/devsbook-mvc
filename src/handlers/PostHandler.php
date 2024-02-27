851ae8b84e9b35f795f5f6ed2034fa08cb9d73ea
alecio70 <alecioccb70@gmail.com>
2024-02-27 13:26:47 -0300

    adding the profile and making route, menu, basic content, specific content, feed, and making the exit button

--- src/handlers/PostHandler.php
+++ src/handlers/PostHandler.php
@@ -19,6 +19,68 @@ class PostHandler {
         }
     }
 
+    public static function _postListToObject($postList, $loggedUserId) {
+        $posts = [];
+
+        foreach ($postList as $postItem) {
+            $newPost = new Post();
+            $newPost->id = $postItem['id'];
+            $newPost->type = $postItem['type'];
+            $newPost->create_at = $postItem['create_at'];
+            $newPost->body = $postItem['body'];
+            $newPost->mine = false;
+
+            if ($postItem['id_user'] === $loggedUserId) {
+                $newPost->mine = true;
+            }
+
+            //4. preencher as informações adicionais no post
+            $newUser = User::select()->where('id', $postItem['id_user'])->one();
+            $newPost->user = new User();
+            $newPost->user->id = $newUser['id'];
+            $newPost->user->name = $newUser['name'];
+            $newPost->user->avatar = $newUser['avatar'];
+            
+            //4.1 preencher as informações de LIKE
+            $newPost->likeCount = 0;
+            $newPost->liked = false;
+            //4.2 preencher as informações de COMMENTS
+            $newPost->comments = [];
+            
+            $posts[] = $newPost;
+        }
+
+        return $posts;
+    }
+
+    public static function getUserFeed($idUser, $page, $loggedUserId) {
+        $serPage = 2;
+
+        //2. pegar os posts dessa galera ordenado pela data.
+        $postList = Post::select()
+            ->where('id_user', $idUser)
+            ->orderBy('create_at', 'desc')
+            ->page($page, $serPage)
+            
+        ->get();
+
+        $total = Post::select()
+            ->where('id_user', $idUser)
+        ->count();
+
+        $pageCount = ceil($total / $serPage);
+
+        //3. transformar resultado em objetos dos models
+        $posts = self::_postListToObject($postList, $loggedUserId);
+
+        //5. retornar o resultado
+        return [
+            'posts' => $posts, 
+            'pageCount' => $pageCount,
+            'currentPage' => $page
+        ];
+    }
+
     public static function getHomeFeed($idUser, $page) {
         $serPage = 2;
 
@@ -47,35 +109,7 @@ class PostHandler {
         $pageCount = ceil($total / $serPage);
 
         //3. transformar resultado em objetos dos models
-        $posts = [];
-
-        foreach ($postList as $postItem) {
-            $newPost = new Post();
-            $newPost->id = $postItem['id'];
-            $newPost->type = $postItem['type'];
-            $newPost->create_at = $postItem['create_at'];
-            $newPost->body = $postItem['body'];
-            $newPost->mine = false;
-
-            if ($postItem['id_user'] === $idUser) {
-                $newPost->mine = true;
-            }
-
-            //4. preencher as informações adicionais no post
-            $newUser = User::select()->where('id', $postItem['id_user'])->one();
-            $newPost->user = new User();
-            $newPost->user->id = $newUser['id'];
-            $newPost->user->name = $newUser['name'];
-            $newPost->user->avatar = $newUser['avatar'];
-
-            //4.1 preencher as informações de LIKE
-            $newPost->likeCount = 0;
-            $newPost->liked = false;
-            //4.2 preencher as informações de COMMENTS
-            $newPost->comments = [];
-
-            $posts[] = $newPost;
-        }
+        $posts = self::_postListToObject($postList, $idUser);
 
         //5. retornar o resultado
         return [
@@ -83,6 +117,27 @@ class PostHandler {
             'pageCount' => $pageCount,
             'currentPage' => $page
         ];
+    }
+
+    public static function getPhotosFrom($idUser) {
+        $photosData = Post::select()
+            ->where('id_user', $idUser)
+            ->where('type', 'photo')
+        ->get();
+
+        $photos = [];
+
+        foreach ($photosData as $photo) {
+            $newPost = new Post();
+            $newPost->id = $photo['id'];
+            $newPost->type = $photo['type'];
+            $newPost->create_at = $photo['create_at'];
+            $newPost->body = $photo['body'];
+
+            $photos[] = $newPost;
+        }
+
+        return $photos;
     }
 
 }
